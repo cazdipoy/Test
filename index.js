a21ef20cea2f596cbc78bb857706e5d8de5aa89a
cazdipoy <cazdipoy@gmail.com>
2026-02-12 16:54:21 +0300

    initialday1

---
+++ index.js
@@ -0,0 +1,30 @@
+// console.log("Cazdi");
+
+//var let const
+
+// var name = "abdi";
+// console.log(name);
+
+// let h=19
+// console.log(h);
+
+// let j="gabow"
+// console.log(j);
+
+
+// let a = 10;
+// a = 27;
+// console.log(a);
+
+
+let name = ["abdi", "hassan", "mohamed", "Cazdi","Siciid"];
+name.pop("Siciid");
+console.log(name);
+let m = ["abdi", "hassan", "mohamed", "Cazdi","Siciid"];
+m.push("abdirahman");
+console.log(m);
+function sum(t,m){
+    return t+m;
+}
+console.log(sum(10,9));
+
